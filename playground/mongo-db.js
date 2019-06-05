8785e03969472beacbff8ecf30ad722b1d1700d8
Stanley Ndunwere <StanleyNdunwere@gmail.com>
2019-06-05 15:59:19 +0100

    added update function

--- playground/mongo-db.js
+++ playground/mongo-db.js
@@ -23,46 +23,56 @@
 //
 const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp',(err, client)=>{
-    if(err){
+MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp', (err, client) => {
+    if (err) {
         console.log('couldn\'t connnect to db');
-    }else{
+    } else {
         console.log('connected successfully');
         const db = client.db('TodoApp');
         db.collection('Users').insertOne({
             name: 'Jothrew Mead',
             age: '25',
             location: 'kansas'
-        }, (err, result)=>{
-           if(err){
-               console.log('failed to insert document');
-           } else{
-               console.log('document inserted successfully');
-               console.log(result.ops[0]._id.getTimestamp());
-           }
+        }, (err, result) => {
+            if (err) {
+                console.log('failed to insert document');
+            } else {
+                console.log('document inserted successfully');
+                console.log(result.ops[0]._id.getTimestamp());
+            }
         });
-        async function awaitResult (){
+
+        async function awaitResult() {
             const docs = await db.collection('Users').find({name: 'Jothrew Mead'}).toArray();
             const jsonDocs = JSON.stringify(docs);
             console.log(jsonDocs);
         }
-        async function awaitResultCount (){
+
+        async function awaitResultCount() {
             const value = await db.collection('Users').find().count();
             const jsonVal = JSON.stringify(value);
             console.log(jsonVal);
         }
 
-        async function findOneAndDelete (){
+        async function findOneAndDelete() {
             const value = await db.collection('Users').findOneAndDelete({name: "Andrew Mead"});
             console.log(value);
         }
 
+        async function findOneAndUpdate() {
+            const value = await db.collection('Users').findOneAndUpdate({name: "Jothrew Mead"},
+                {$set: {name: 'Jerrow Tlingaafe'}},
+                {returnOriginal: false});
+            console.log(value);
+        }
+
+
+        findOneAndUpdate()
         findOneAndDelete()
         // awaitResultCount();
         // awaitResult();
 
 
-
-    client.close();
+        client.close();
     }
 });
